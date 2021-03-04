20b70aa37c047a4ccceb5ffdb6a434c84ce33eb8
jaeltg <jaeltg@hotmail.com>
2021-03-04 13:32:31 +0000

    added first four tests

--- high_scores/tests/high_scores_test.py
+++ high_scores/tests/high_scores_test.py
@@ -8,15 +8,24 @@ from src.high_scores import latest, personal_best, personal_top_three
 class HighScoresTest(unittest.TestCase):
     
     # Tests
+    def setUp(self):
+        self.scores = [10, 11, 100, 42, 21, 29, 59, 83, 1]
 
     # Test latest score (the last thing in the list)
+    def test_can_get_latest_score(self):
+        self.assertEqual(1, latest(self.scores))
 
     # Test personal best (the highest score in the list)
+    def test_find_personal_best_score(self):
+        self.assertEqual(100, personal_best(self.scores))
 
     # Test top three from list of scores
-
+    def test_can_find_top_three(self):
+        self.assertEqual([100, 83, 59], personal_top_three(self.scores))
+        
     # Test ordered from highest tp lowest
-
+    def test_can_sort_highest_to_lowest(self):
+        self.assertEqual([100, 83, 59, 42, 29, 21, 11, 10, 1], sort_scores_highest_to_lowest(self.scores))
     # Test top three when there is a tie
 
     # Test top three when there are less than three
