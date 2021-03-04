3e2c927e36c37d51da107a600228d0063b0565a6
jaeltg <jaeltg@hotmail.com>
2021-03-04 15:30:18 +0000

    created tests for compound interest

--- compound_interest/tests/compound_interest_test.py
+++ compound_interest/tests/compound_interest_test.py
@@ -7,15 +7,29 @@ class CompoundInterestTest(unittest.TestCase):
     # Tests
 
     # Should return 732.81 given 100 principal, 10 percent, 20 years
+    def test_returns_compound_interest__scenario_1(self):
+        compound_interest = CompoundInterest(100, 10, 20)
+        self.assertEqual(732.81, compound_interest.calculate_final_investment())
 
     # Should return 181.94 given 100 principal, 6 percent, 10 years
+    def test_returns_compound_interest__scenario_2(self):
+        compound_interest = CompoundInterest(100, 6, 10)
+        self.assertEqual(181.94, compound_interest.calculate_final_investment())
 
     # Should return 149,058.55 given 100000 principal, 5 percent, 8 years
+    def test_returns_compound_interest__scenario_2(self):
+        compound_interest = CompoundInterest(100000, 5, 8)
+        self.assertEqual(149,058.55, compound_interest.calculate_final_investment())
 
     # Should return 0.00 given 0 principal, 10 percent, 1 year
+    def test_returns_compound_interest__scenario_2(self):
+        compound_interest = CompoundInterest(0, 10, 1)
+        self.assertEqual(0.00, compound_interest.calculate_final_investment())
 
     # Should return 100.00 given 100 principal, 0 percent, 10 years
-
+    def test_returns_compound_interest__scenario_2(self):
+        compound_interest = CompoundInterest(100, 0, 10)
+        self.assertEqual(100.00, compound_interest.calculate_final_investment())
 
     # Extention tests
 
